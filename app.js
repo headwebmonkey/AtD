2f7b63dea237a768fb580f957e7e04ef0bcfa232
Kody Peterson <kody.peterson@disney.com>
2013-10-28 15:18:51 -0400

    Initial Routing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 1049);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -28,7 +28,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/checkDocument', routes.index);
+app.get('/checkGrammar', routes.index);
+app.get('/stats', routes.index);
+app.get('/verify', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
